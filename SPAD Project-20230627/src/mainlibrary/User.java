f2a95d21771ee5b8ab25c4605d74d3d0deff7714
SamitaRamesh <samitaramesh9@gmail.com>
2023-08-14 15:32:25 +0530

    Initial Commit

---
+++ SPAD Project-20230627/src/mainlibrary/User.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mainlibrary;
+
+/**
+ *
+ * @author Samita
+ */
+class User {
+
+    User(String userName, String password, String position, String email, String program, int year) {
+    }
+    
+}
